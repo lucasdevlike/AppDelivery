c1b25c8fa0530d93b78defe9f0a5152de85b0662
lucasfregonezi <lucasfregonezi@gmail.com>
2024-07-09 01:26:18 -0300

    App Finalizado

---
+++ src/components/header/index.tsx
@@ -0,0 +1,28 @@
+import { View, Pressable, Text } from 'react-native';
+import { Ionicons, Feather } from '@expo/vector-icons';
+
+
+
+
+export function Header() {
+    return (
+        <View className="w-full flex flex-row items-center justify-between">
+            <Pressable className="w-10 h-10 bg-white justify-center items-center rounded-full flex">
+                <Ionicons name="menu" size={20} color="#121212" />
+            </Pressable>
+
+            <View className="flex flex-col items-center justify-center">
+                <Text className="text-center text-sm text-slate-700">Localização</Text>
+
+                <View className="flex-row items-center justify-center gap-1">
+                    <Feather name="map-pin" size={14} color="#ff0000" />
+                    <Text className='text-lg font-bold'>Ribeirão Preto</Text>
+                </View>
+            </View>
+
+            <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+                <Feather name="bell" size={20} color="#121212" />
+            </Pressable>
+        </View>
+    );
+}
